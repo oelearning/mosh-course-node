83e277ff8c316c35b294048cba3eff9357cafd08
Oscar Estrada <arturostrada2020@gmail.com>
2022-08-17 13:37:10 -0500

    create get and post endpoints

--- index.js
+++ index.js
@@ -1,22 +1,63 @@
+const Joi = require('joi')
 const express = require('express')
 const app = express()
-require('dotenv').config()
 
+require('dotenv').config()
 const PORT = process.env.PORT || 3000
 
+const clients = [
+  { name: 'Cesar', age: 25, id: 1 },
+  { name: 'Laura', age: 30, id: 2 },
+  { name: 'Victor', age: 23, id: 3 },
+  { name: 'Oscar', age: 27, id: 4 }
+]
+
 const requestTime = function (req, res, next) {
-  req.requestTime = Date.now()
+  const requestTime = new Date(Date.now()).toString()
+  console.log(`Requested at: ${requestTime}`)
   next()
 }
 
 app.use(requestTime)
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  let responseText = 'Hello World!<br>'
-  const dateFormat = new Date(req.requestTime).toString()
-  responseText += `<small>Requested at: ${dateFormat}</small>`
-  console.log(`Requested at: ${dateFormat}`)
-  res.send(responseText)
+// Get all clients
+app.get('/clients', (req, res) => {
+  res.send(clients)
+})
+
+// Get client by id
+app.get('/clients/:id', (req, res) => {
+  const client = clients.find(item => item.id === parseInt(req.params.id))
+  if (!client) return res.status(404).send('The client with the given id was not found')
+  res.send(client)
+})
+
+app.post('/clients', (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+    age: Joi.number().required()
+  })
+
+  const result = schema.validate(req.body)
+  console.log(result)
+
+  if (result.error) return res.status(400).send(result.error.details[0].message)
+
+  const client = {
+    id: clients.length + 1,
+    name: req.body.name,
+    age: req.body.age
+  }
+
+  clients.push(client)
+  console.log('Client was created successfully')
+  res.send(
+    {
+      message: 'client was created successfully',
+      client
+    }
+  )
 })
 
 app.listen(PORT, () => {
